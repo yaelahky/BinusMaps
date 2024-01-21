a60d840022b252cfa0f40b2b7289b574653a7827
yaelahky <kiky.lenovo@gmail.com>
2024-01-21 18:16:26 +0700

    delete unused code

--- src/screens/Home/useHome.js
+++ src/screens/Home/useHome.js
@@ -1,33 +1,9 @@
-import {useEffect, useState} from 'react';
-import {useNavigation} from '@react-navigation/native';
-import {getAllNotes} from '../../helpers';
-
 const useHome = () => {
-  const navigation = useNavigation();
-
-  const [data, setData] = useState([]);
-
   const userName = 'Binusian';
 
-  const getData = async () => {
-    const dataNotes = await getAllNotes();
-    setData(dataNotes);
-  };
-
-  const navigateToEdit = itemData => {
-    navigation.navigate('EditNote', {itemData, onGoBack: getData});
-  };
-
-  useEffect(() => {
-    getData();
-  }, [navigation]);
-
   return {
-    nav: {
-      navigateToEdit,
-    },
+    nav: {},
     state: {
-      data,
       userName,
     },
     func: {},
